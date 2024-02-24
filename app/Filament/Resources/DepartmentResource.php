0eb09a2745927176cb08df93ab39715b400515d2
Nehal Patel <iamnehalpatel@gmail.com>
2024-02-24 18:07:45 +0530

    Relationship Managers created

--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources;
 
+use App\Filament\Resources\DepartmentResource\RelationManagers\EmployeesRelationManager;
 use App\Filament\Resources\DepartmentResource\Pages;
 use App\Filament\Resources\DepartmentResource\RelationManagers;
 use App\Models\Department;
@@ -87,7 +88,7 @@ class DepartmentResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            EmployeesRelationManager::class
         ];
     }
 
---
+++ app/Filament/Resources/StateResource/RelationManagers/EmployeesRelationManager.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Filament\Resources\StateResource\RelationManagers;
+
+use App\Models\City;
+use App\Models\State;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Collection;
+
+class EmployeesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'employees';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('User Details')
+                    ->description('Provide User details')
+                    ->schema([
+                        TextInput::make('first_name')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('last_name')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('middle_name')
+                            ->maxLength(255),
+                    ])->columns(3),
+                Section::make('User Address')
+                    ->description('Provide Address Details')
+                    ->schema(array(
+                        Select::make('country_id')
+                            ->relationship(name: 'country', titleAttribute: 'name')
+                            ->required()
+                            ->preload()
+                            ->live()
+                            ->afterStateUpdated(function (Set $set) {
+                                $set('state_id', null);
+                                $set('city_id', null);
+                            })
+                            ->searchable(),
+                        Select::make('state_id')
+                            ->options(options: fn(Get $get): Collection => State::query()
+                                ->where('country_id', $get('country_id'))
+                                ->pluck('name', 'id'))
+                            ->required()
+                            ->afterStateUpdated(fn(Set $set) => $set('city_id', null))
+                            ->live()
+                            ->searchable(),
+                        Select::make('city_id')
+                            ->options(options: fn(Get $get): Collection => City::query()
+                                ->where('state_id', $get('state_id'))
+                                ->pluck('name', 'id'))
+                            ->required()
+                            ->live()
+                            ->searchable(),
+                        Select::make('department_id')
+                            ->relationship('department', titleAttribute: 'name')
+                            ->required()
+                            ->preload()
+                            ->searchable(),
+                        TextInput::make('address')
+                            ->maxLength(255),
+                        TextInput::make('zipcode')
+                            ->maxLength(255),
+                    ))->columns(3),
+                Forms\Components\Section::make('Dates')
+                    ->schema([
+                        DatePicker::make('date_of_birth')
+                            ->displayFormat('d/M/Y')
+                            ->native(true),
+                        DatePicker::make('date_hired')
+                            ->displayFormat('d/M/Y')
+                            ->native(true),
+                    ])->columns(2)
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('first_name')
+            ->columns([
+                TextColumn::make('department.name')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('first_name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('last_name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('middle_name')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('address')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('zipcode')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('country.name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('date_of_birth')
+                    ->date()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('date_hired')
+                    ->date()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
